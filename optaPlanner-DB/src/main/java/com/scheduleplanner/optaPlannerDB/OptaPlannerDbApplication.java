7d6e1ced224e3430db043d8c04880325440d77a0
itMeansBigMountain <affan.fareed@gmail.com>
2022-09-03 17:17:52 -0500

    INIT - REST db & React.js client

---
+++ optaPlanner-DB/src/main/java/com/scheduleplanner/optaPlannerDB/OptaPlannerDbApplication.java
@@ -0,0 +1,13 @@
+package com.scheduleplanner.optaPlannerDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptaPlannerDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptaPlannerDbApplication.class, args);
+	}
+
+}
